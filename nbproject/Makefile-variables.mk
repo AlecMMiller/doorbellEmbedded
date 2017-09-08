6dcdb587da60460f209a5afa1c1fa9ea47bd3aa0
Alec Miller <Alec.Miller@microchip.com>
2017-09-08 11:14:26 -0700

    Added README

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=doorbellMigrated.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/doorbellMigrated.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=doorbellmigrated.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/doorbellmigrated.tar
